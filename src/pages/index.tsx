07ef19b2230535d68f71d661dc63056bb59f00a8
bpognon <bastien.pognon@smsmode.com>
2025-07-04 14:25:55 +0200

    feat : add component on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import OpenGate from "@/components/OpenGate";
 import PublicConcertList from "@/components/PublicConcertList";
+import FullWidthEmbed from "@/components/ClipVideo";
 
 const IndexPage: React.FC = () => {
   return (
     <div className="custom-cursor">
       <OpenGate />
+      <FullWidthEmbed />
       <PublicConcertList concerts={[]} />
     </div>
   );
