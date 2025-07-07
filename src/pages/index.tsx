6ba049584315083ad3ac614ef46063ec17fc473a
bpognon <bastien.pognon@smsmode.com>
2025-07-07 12:22:47 +0200

    feat: add component to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import OpenGate from "@/components/OpenGate";
 import PublicConcertList from "@/components/PublicConcertList";
 import FullWidthEmbed from "@/components/ClipVideo";
-
+import Countdown from "@/components/CountDown";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 const IndexPage: React.FC = () => {
   return (
-    <div className="custom-cursor">
+    <div className="custom-cursor bg-black">
+      <Header />
       <OpenGate />
       <FullWidthEmbed />
+      <Countdown />
       <PublicConcertList concerts={[]} />
+      <Footer />
     </div>
   );
 };
