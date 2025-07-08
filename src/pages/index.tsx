858e2f4bdd0d85e9e37afa1fb5a677fab65db63c
bpognon <bastien.pognon@smsmode.com>
2025-07-08 11:51:31 +0200

    feat : added all components to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,21 @@ import FullWidthEmbed from "@/components/ClipVideo";
 import Countdown from "@/components/CountDown";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import AlbumSection from "@/components/AlbumSection";
+import SongList from "@/components/SongList";
+import PublicNewsList from "@/components/PublicNewsList";
+
 const IndexPage: React.FC = () => {
   return (
-    <div className="custom-cursor bg-black">
+    <div className="bg-black">
       <Header />
       <OpenGate />
       <FullWidthEmbed />
+      <SongList />
       <Countdown />
-      <PublicConcertList concerts={[]} />
+      <AlbumSection />
+      <PublicConcertList />
+      <PublicNewsList />
       <Footer />
     </div>
   );
